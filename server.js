1b00f23c67716ee3573b1a606655b577ba6407c1
mroz22 <mvarmuza@gmail.com>
2020-06-02 11:57:41 +0200

    fix

--- server.js
+++ server.js
@@ -1,9 +1,7 @@
 const express = require('express');
-const path = require('path');
 const bodyParser = require('body-parser');
 const cors = require('cors');
 const mongoose = require('mongoose');
-const socket = require('socket.io');
 
 const config = require('./config/db');
 
