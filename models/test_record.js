0bf8d1e4d455efe7f194b6548b89e989e54cc917
mroz22 <mvarmuza@gmail.com>
2020-10-14 08:30:43 +0200

    foo

--- models/test_record.js
+++ models/test_record.js
@@ -1,30 +1,39 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
-const TestRecordSchema = new mongoose.Schema({
-  jobUrl: {
-    type: String,
+const TestRecordSchema = new mongoose.Schema(
+  {
+    jobUrl: {
+      type: String,
+    },
+    jobId: {
+      type: String,
+    },
+    branch: {
+      type: String,
+    },
+    commitMessage: {
+      type: String,
+    },
+    commitSha: {
+      type: String,
+    },
+    stage: [String],
+    records: {
+      type: mongoose.Schema.Types.Mixed,
+    },
+    duration: {
+      type: Number,
+    },
+    pipelineUrl: {
+      type: String,
+    },
   },
-  jobId: {
-    type: String,
-  },
-  branch: {
-    type: String,
-  },
-  commitMessage: {
-    type: String,
-  },
-  commitSha: {
-    type: String,
-  },
-  stage: [String],
-  records: {
-    type: mongoose.Schema.Types.Mixed,
-  },
-  duration: {
-    type: Number,
+  {
+    capped: { size: 2000000, max: 400, autoIndexId: true },
   }
-}, {
-   capped: { size: 2000000, max: 400, autoIndexId: true }
-});
+);
 
-const TestRecord = module.exports = mongoose.model('test_record', TestRecordSchema);
+const TestRecord = (module.exports = mongoose.model(
+  "test_record",
+  TestRecordSchema
+));
