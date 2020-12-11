76ffba574eb2e1ab5c0a40becade75f78cf76b00
mroz22 <mvarmuza@gmail.com>
2020-12-11 12:44:07 +0100

    build

--- react-src/src/components/Detail/index.js
+++ react-src/src/components/Detail/index.js
@@ -11,68 +11,94 @@ const Detail = ({ data }) => {
     return "No luck here";
   }
 
-  return Object.entries(data[commit].stages).map(([stage, data]) => {
-    return (
-      // <div>
-      //   <h3>{stage}</h3>
-      //   {(!data.screenshots || !data.screenshots.length) && "No screenshots"}
-      //   {data.screenshots &&
-      //     data.screenshots.map((screenshot) => (
-      //       <div>
-      //         <div>{screenshot.name}</div>
-      //         <img
-      //           style={{
-      //             display: 'block',
-      //             marginLeft: 'auto',
-      //             marginRight: 'auto',
-      //             border: "4px dotted pink",
-      //           }}
-      //           src={`${data.jobUrl}${screenshot.path.replace(
-      //             "trezor-suite",
-      //             "artifacts/raw"
-      //           )}`}
-      //         />
-      //       </div>
-      //     ))}
-      // </div>
-      <div>
-        <h1>{stage}</h1>
-        {(!data.tests || !data.tests.length) && "Nothing here"}
-        {data.tests &&
-          data.tests.map((test) => (
+  const stateToColor = (state) => {
+    switch (state) {
+      case "passed":
+        return "green";
+      case "pending":
+        return "blue";
+      case "failed":
+        return "red";
+      default:
+        return "pink";
+    }
+  };
+
+  // todo: add link to video
+  // href={`${data[pipeline].stages[stage].jobUrl}/artifacts/file/packages/integration-tests/projects/suite-web/videos/${record}.test.ts.mp4`}
+
+  return (
+    <div>
+      {/* index??? */}
+      {/* {Object.entries(data[commit].stages).map(([stage, data]) => {
+        return (
+          <div>
             <div>
-              {test.attempts.map((attempt) => (
-                <div
-                  style={{
-                    backgroundColor:
-                      attempt.state === "passed" ? "green" : "red",
-                  }}
-                >
-                  <h3>{test.title.join(">>")}</h3>
-                  {attempt.screenshots.map((screenshot) => (
-                    <div>
-                      <div>{screenshot.name}</div>
-                      <img
-                        style={{
-                          display: "block",
-                          marginLeft: "auto",
-                          marginRight: "auto",
-                          border: "4px dotted pink",
-                        }}
-                        src={`${data.jobUrl}${screenshot.path.replace(
-                          "trezor-suite",
-                          "artifacts/raw"
-                        )}`}
-                      />
+              {stage}
+            </div>
+            {(!data.tests || !data.tests.length) && "Nothing here"}
+            {data.tests &&
+              data.tests.map((test) => (
+                <div>
+                  {test.attempts.map((attempt) => (
+                    <div
+                      style={{
+                        color: stateToColor(attempt.state),
+                      }}
+                    >
+                      <a href={`#${test.title.join(">>")}`}>{test.title.join(" >> ")}</a>
                     </div>
                   ))}
                 </div>
               ))}
-            </div>
-          ))}
-      </div>
-    );
-  });
+          </div>
+        );
+      })} */}
+      {Object.entries(data[commit].stages).map(([stage, data]) => {
+        return (
+          <div>
+            <h1>
+              <a href={data.jobUrl}>{stage}</a>
+            </h1>
+            {(!data.tests || !data.tests.length) && "Nothing here"}
+            {data.tests &&
+              data.tests.map((test) => (
+                <div>
+                  {test.attempts.map((attempt) => (
+                    <div
+                      style={{
+                        backgroundColor: stateToColor(attempt.state),
+                      }}
+                    >
+                      <h3 id={test.title.join(">>")}>
+                        {test.title.join(" >> ")}
+                      </h3>
+                      {attempt.screenshots.map((screenshot) => (
+                        <div>
+                          <div>{screenshot.name}</div>
+                          <img
+                            style={{
+                              display: "block",
+                              marginLeft: "auto",
+                              marginRight: "auto",
+                              border: "4px dotted pink",
+                            }}
+                            src={`${data.jobUrl}${screenshot.path.replace(
+                              "trezor-suite",
+                              "artifacts/raw"
+                            )}`}
+                          />
+                        </div>
+                      ))}
+                    </div>
+                  ))}
+                </div>
+              ))}
+          </div>
+        );
+      })}
+    </div>
+  );
 };
 
 export default Detail;
