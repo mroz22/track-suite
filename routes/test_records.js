cff612cab47a89b278082931cf67185cba09a1c6
Martin Varmuza <mvarmuza@gmail.com>
2024-04-15 13:24:37 +0200

    fix replace

--- routes/test_records.js
+++ routes/test_records.js
@@ -1,11 +1,10 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
 
-const TestRecord = require('../models/test_record');
+const TestRecord = require("../models/test_record");
 
 // READ (ALL)
-router.get('/', (req, res) => {
- 
+router.get("/", (req, res) => {
   const { branch } = req.query;
   TestRecord.find({ branch })
     .then((result) => {
@@ -13,25 +12,34 @@ router.get('/', (req, res) => {
     })
     .catch((err) => {
       console.log(err);
-      res.status(500).json({ success: false, msg: `Something went wrong. ${err}` });
+      res
+        .status(500)
+        .json({ success: false, msg: `Something went wrong. ${err}` });
     });
 });
 
-router.get('/branches', (req, res) => {
-  TestRecord.distinct('branch').exec().then(result => {
-    res.json(result);
-  }).catch(err => {
-    console.log(err);
-    res.status(500).json({ success: false, msg: `Something went wrong. ${err}` });
-  }) 
-})
-
-
+router.get("/branches", (req, res) => {
+  TestRecord.distinct("branch")
+    .exec()
+    .then((result) => {
+      res.json(result);
+    })
+    .catch((err) => {
+      console.log(err);
+      res
+        .status(500)
+        .json({ success: false, msg: `Something went wrong. ${err}` });
+    });
+});
 
 // CREATE
-router.post('/', (req, res) => {
-  let newRecord = new TestRecord({ ...req.body, branch: req.body.branch.replace('/', '-')});
-  newRecord.save()
+router.post("/", (req, res) => {
+  let newRecord = new TestRecord({
+    ...req.body,
+    branch: req.body.branch.replaceAll("/", "-"),
+  });
+  newRecord
+    .save()
     .then((result) => {
       res.json({
         success: true,
@@ -43,7 +51,9 @@ router.post('/', (req, res) => {
       console.log(err);
       if (err.errors) {
         // Show failed if all else fails for some reasons
-        res.status(500).json({ success: false, msg: `Something went wrong. ${err}` });
+        res
+          .status(500)
+          .json({ success: false, msg: `Something went wrong. ${err}` });
       }
     });
 });
